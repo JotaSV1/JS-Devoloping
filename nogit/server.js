6fcb66b03868a9b81b7f018ef9f23ba12fef73e4
joao <joaomiguelsv1@gmail.com>
2024-10-25 20:49:48 -0300

    .

--- nogit/server.js
+++
@@ -1,8 +0,0 @@
-import { createServer } from 'node:http' //visivelmente, isso importa o servidor do node
-const app = createServer
-
-const server = createServer(() => {
-    console.log('vantuba')
-})
-
-server.listen(3232)     //localhost > 3232
